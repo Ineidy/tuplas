fcd71169d94c79ff77e2febbef7a109f63cc158e
Ineidy <160775201+Ineidy@users.noreply.github.com>
2024-02-26 15:10:16 +0100

    Tuplas

---
+++ tuplas.py
@@ -0,0 +1,12 @@
+nombre =input("Escribe tu nombre completo: ")
+edad =input("Escribe tu edad: ")
+cursos =input("Escribe tus cursos: ")
+direccion =input("Escribe tu direccion: ")
+ExperienciaLaboral =input("Escribe tu experiencia laboral: ")
+
+informacion= tuple((nombre, edad, cursos, direccion, ExperienciaLaboral))
+print(informacion)
+
+#muchos estaban diciendo que eran 4 tuplas en total pero segun yo es asi jaja 
+#ya que una tupla almacena toda la informacion y es lo que se intenta
+#que en una sola tupla este toda la informacion de una persona
